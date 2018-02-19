c3e660959c7923feba19edd17c8b7e7277948089
Eli Artz <Artz0801@student.cptc.edu>
2018-02-19 14:51:14 -0800

    Add project files.

---
+++ TeamFileIOAssignment/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TeamFileIOAssignment
+{
+    partial class frmImportFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnImportFile = new System.Windows.Forms.Button();
+            this.btnEncryptFile = new System.Windows.Forms.Button();
+            this.btnDecryptFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReadAndDisplayFile = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(76, 31);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(190, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnImportFile
+            // 
+            this.btnImportFile.Location = new System.Drawing.Point(279, 31);
+            this.btnImportFile.Name = "btnImportFile";
+            this.btnImportFile.Size = new System.Drawing.Size(75, 23);
+            this.btnImportFile.TabIndex = 1;
+            this.btnImportFile.Text = "Import";
+            this.btnImportFile.UseVisualStyleBackColor = true;
+            this.btnImportFile.Click += new System.EventHandler(this.btnImportFile_Click);
+            // 
+            // btnEncryptFile
+            // 
+            this.btnEncryptFile.Location = new System.Drawing.Point(55, 239);
+            this.btnEncryptFile.Name = "btnEncryptFile";
+            this.btnEncryptFile.Size = new System.Drawing.Size(92, 23);
+            this.btnEncryptFile.TabIndex = 3;
+            this.btnEncryptFile.Text = "Encrypt File";
+            this.btnEncryptFile.UseVisualStyleBackColor = true;
+            this.btnEncryptFile.Click += new System.EventHandler(this.btnEncryptFile_Click);
+            // 
+            // btnDecryptFile
+            // 
+            this.btnDecryptFile.Location = new System.Drawing.Point(217, 239);
+            this.btnDecryptFile.Name = "btnDecryptFile";
+            this.btnDecryptFile.Size = new System.Drawing.Size(88, 23);
+            this.btnDecryptFile.TabIndex = 4;
+            this.btnDecryptFile.Text = "Decrypt File";
+            this.btnDecryptFile.UseVisualStyleBackColor = true;
+            this.btnDecryptFile.Click += new System.EventHandler(this.btnDecryptFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(76, 279);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(86, 23);
+            this.btnSaveFile.TabIndex = 5;
+            this.btnSaveFile.Text = "Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "File Text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Import File:";
+            // 
+            // txtReadAndDisplayFile
+            // 
+            this.txtReadAndDisplayFile.Location = new System.Drawing.Point(55, 88);
+            this.txtReadAndDisplayFile.Multiline = true;
+            this.txtReadAndDisplayFile.Name = "txtReadAndDisplayFile";
+            this.txtReadAndDisplayFile.Size = new System.Drawing.Size(250, 135);
+            this.txtReadAndDisplayFile.TabIndex = 9;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(203, 279);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmImportFile
+            // 
+            this.AcceptButton = this.btnImportFile;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(366, 327);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtReadAndDisplayFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnImportFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnDecryptFile);
+            this.Controls.Add(this.btnEncryptFile);
+            this.Name = "frmImportFile";
+            this.Text = "Import A File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnImportFile;
+        private System.Windows.Forms.Button btnEncryptFile;
+        private System.Windows.Forms.Button btnDecryptFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReadAndDisplayFile;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TeamFileIOAssignment/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamFileIOAssignment
+{
+    public partial class frmImportFile : Form
+    {
+        public frmImportFile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncryptFile_Click(object sender, EventArgs e)
+        {
+            EncryptFile();
+        }
+        
+        private void EncryptFile()
+        {
+            try
+            {
+                string fileText = txtReadAndDisplayFile.Text;
+                byte[] byteCode = Encoding.UTF8.GetBytes(fileText);
+                string returnText = Convert.ToBase64String(byteCode);
+                txtReadAndDisplayFile.Text = returnText;
+            }
+            catch
+            {
+                MessageBox.Show("File is already encrypted.");
+            }
+        }
+
+        private void btnDecryptFile_Click(object sender, EventArgs e)
+        {
+            DecryptFile();
+        }
+
+        private void DecryptFile()
+        {
+            try
+            {
+                string fileText = txtReadAndDisplayFile.Text;
+                byte[] byteCode = Convert.FromBase64String(fileText);
+                string returnText = Encoding.UTF8.GetString(byteCode);
+                txtReadAndDisplayFile.Text = returnText;
+            }
+            catch
+            {
+                MessageBox.Show("The file was already decrypted.");
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.ShowDialog();
+
+            string fileName = saveFile.FileName;
+            string textToDisplay = txtReadAndDisplayFile.Text;
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = new FileStream(fileName, FileMode.Create);
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    writer.Write(textToDisplay);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("You didn't save the file!");
+            }
+            finally
+            {
+                if (fileStream != null)
+                {
+                    fileStream.Dispose();
+                }
+            }
+        }
+
+        private void btnImportFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog importFileName = new OpenFileDialog();
+            importFileName.ShowDialog();
+
+            //show file name in text box
+            txtFileName.Text = importFileName.FileName;
+
+            DisplayFile();
+        }
+
+        private void DisplayFile()
+        {
+            // display the file info in the text box
+            try
+            {   // open the text file
+                using (StreamReader streamReader = new StreamReader(txtFileName.Text))
+                {
+                    String lineToRead = streamReader.ReadToEnd();
+                    txtReadAndDisplayFile.Text = lineToRead;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("The file you have chosen could not be read.");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
